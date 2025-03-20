46e3f73ff7252189d4ed63c034c3440824772d43
UzairBinAsif <shaikhuzairasif14@gmail.com>
2025-03-20 23:55:10 +0500

    update for non-negative condition

--- problem3.py
+++ problem3.py
@@ -6,10 +6,10 @@ Example:
 🔹 Output: 10
 💡 Hint: Convert the number to a string and iterate over each digit or use modulus and division.
 '''
-def sum_digit(n: str) -> int:
+def sum_digit(n: str) -> int | str:
     count_sum: int = 0
     if int(n) < 0:
-        return f'{n} must be a non-negative number'
+        return f'{n} must be a non-negative number.'
     else:
         for i in n:
             count_sum += int(i)
